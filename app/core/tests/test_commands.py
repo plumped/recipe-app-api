377ab9ea45d218573acda150494af7ab203f8e9a
5yn74x <41291962+plumped@users.noreply.github.com>
2022-10-11 20:10:13 +0200

    added commands and tests3

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,20 @@
+"""
+Test custom Django management commands
+"""
+from curses.ascii import SI
+from unittest.mock import patch
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+    """Test commands"""
+
+    def test_wait_for_db_ready(self):
+        """Test waiting for database if database ready."""
+        patched_check.return_value = True
+        call_command('wait_for_db')
